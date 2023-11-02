6e76612d50d43a4c61d43eb7da8cd8e1d14c26e6
Sergio Lopez <sergio9961937@gmail.com>
2023-11-02 00:09:51 -0400

    Added user template.

---
+++ app/templates/Users/form.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\User $user
+ * @var string $action 'add' o 'edit'
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?php if (isset($user->id)): ?>
+                <?= $this->Form->postLink(
+                    __('Delete'),
+                    ['action' => 'delete', $user->id],
+                    ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
+                ) ?>
+            <?php endif; ?>
+            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="users form content">
+            <?= $this->Form->create($user) ?>
+            <fieldset>
+                <legend>
+                    <?php
+                        if (isset($user->id)) {
+                            echo __('Edit User');
+                        } else {
+                            echo __('Add User');
+                        }
+                    ?>
+                </legend>
+                <?php
+                    echo $this->Form->control('email');
+                    // Show the password field for 'add' action only
+                    if (!isset($user->id)) {
+                        echo $this->Form->control('password');
+                    }
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
